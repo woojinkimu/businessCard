3b4a3b277699169cdb665341c5488a9540c38f76
wooreal <hutjunei@gmail.com>
2021-03-02 17:28:17 +0900

    add loading spinner

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -3,7 +3,7 @@ import Button from '../button/button';
 import styles from './card_edit_form.module.css';
 
 const CardEditForm = ({FileInput, card, updateCard, deleteCard}) => {
-  const {name, company, title, email, message, theme, fileName, fileURL} = card;
+  const {name, company, title, email, message, theme, fileName,} = card;
   const onSubmit = () => {
     deleteCard(card);
   }
--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({imageUploader, name, onFileChange}) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
@@ -9,9 +10,11 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
   }
 
   const onChange = async (event) => {
-    console.log(event.target.files[0]);
+    setLoading(true);
+    // console.log(event.target.files[0]);
     const uploaded = await imageUploader.upload(event.target.files[0]);
-    console.log(uploaded);
+    setLoading(false);
+    // console.log(uploaded);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -28,9 +31,13 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
         name="file"
         onChange={onChange}
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || 'No File'}
-      </button>
+      {
+        !loading && 
+          <button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onButtonClick}>
+            {name || 'No File'}
+          </button>
+      }
+      { loading && <div className={styles.loading}></div> }
     </div>
   );
 };
