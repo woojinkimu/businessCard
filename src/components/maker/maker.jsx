079644c6c3340520af5454c63e8837629fb64d29
wooreal <hutjunei@gmail.com>
2021-03-03 17:19:56 +0900

    add firebase realtime database & sync

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,54 +6,86 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
-const Maker = ({FileInput, authService}) => {
-  const [cards, setCards] = useState({
-    '1': {
-      id: '1',
-      name: 'wooreal1',
-      company: 'com1',
-      title: 'title1',
-      theme: 'dark',
-      email: 'hutjunei@gmail.com',
-      message: 'hi',
-      fileName: 'filename',
-      fileURL: null,
-    },
-    '2': {
-      id: '2',
-      name: 'wooreal2',
-      company: 'com2',
-      title: 'title2',
-      theme: 'light',
-      email: 'hutjunei@gmail.com',
-      message: 'hi',
-      fileName: 'filename',
-      fileURL: null,
-    },
-    '3': {
-      id: '3',
-      name: 'wooreal3',
-      company: 'com3',
-      title: 'title3',
-      theme: 'colorful',
-      email: 'hutjunei@gmail.com',
-      message: 'hi',
-      fileName: 'filename',
-      fileURL: null,
-    },
-  });
+const Maker = ({FileInput, authService, cardRepository}) => {
   const history = useHistory();
+  const historyState = history?.location?.state;
+  const [cards, setCards] = useState({});
+  const [userId, setUserId] = useState(historyState && historyState.id);
+  // const [cards, setCards] = useState({
+  //   '1': {
+  //     id: '1',
+  //     name: 'wooreal1',
+  //     company: 'com1',
+  //     title: 'title1',
+  //     theme: 'dark',
+  //     email: 'hutjunei@gmail.com',
+  //     message: 'hi',
+  //     fileName: 'filename',
+  //     fileURL: null,
+  //   },
+  //   '2': {
+  //     id: '2',
+  //     name: 'wooreal2',
+  //     company: 'com2',
+  //     title: 'title2',
+  //     theme: 'light',
+  //     email: 'hutjunei@gmail.com',
+  //     message: 'hi',
+  //     fileName: 'filename',
+  //     fileURL: null,
+  //   },
+  //   '3': {
+  //     id: '3',
+  //     name: 'wooreal3',
+  //     company: 'com3',
+  //     title: 'title3',
+  //     theme: 'colorful',
+  //     email: 'hutjunei@gmail.com',
+  //     message: 'hi',
+  //     fileName: 'filename',
+  //     fileURL: null,
+  //   },
+  // });
+
   const onLogout = () => {
     authService.logout();
   };
 
+  // login 관련 useEffect
   useEffect(()=> {
     authService.onAuthChange(user => {
-      if(!user){
+      if(user){
+        setUserId(user.uid);
+      }else{
         history.push('/');
       }
     })
   });
+  // useEffect(()=> {
+  //   authService.onAuthChange(user => {
+  //     if(!user){
+  //       history.push('/');
+  //     }
+  //   })
+  // });
+
+  // firebase sync 관련 useEffect
+  // mount or update or userId 가 변경되었을 때
+  useEffect(() => {
+    // component 가 mount or update 시
+    // userId 가 전달 되지 않있다면
+    if(!userId){
+      return;
+    }
+    // component 가 mount or update 시
+    // userId 가 전달 되었다면
+    // syncCards 에 전달할 두번째 인자는 콜백함수(onUpdate)
+    const stopSync = cardRepository.syncCards(userId, cards => {
+      setCards(cards);
+    });
+    // CardRepository.syncCards 안의 return 을 const 로 받아와 실행
+    return () => stopSync();
+  }, [userId]);
 
   const createOrUpdateCard = (card) => {
     // const updated = {...cards};
@@ -64,7 +96,18 @@ const Maker = ({FileInput, authService}) => {
       updated[card.id] = card;
       return updated;
     });
+    cardRepository.saveCard(userId, card);
   };
+  // const createOrUpdateCard = (card) => {
+  //   // const updated = {...cards};
+  //   // updated[card.id] = card;
+  //   // setCards(updated);
+  //   setCards(cards => {
+  //     const updated = {...cards};
+  //     updated[card.id] = card;
+  //     return updated;
+  //   });
+  // };
 
   const deleteCard = (card) => {
     setCards(cards => {
@@ -72,7 +115,15 @@ const Maker = ({FileInput, authService}) => {
       delete updated[card.id];
       return updated;
     });
+    cardRepository.removeCard(userId, card);
   };
+  // const deleteCard = (card) => {
+  //   setCards(cards => {
+  //     const updated = {...cards};
+  //     delete updated[card.id];
+  //     return updated;
+  //   });
+  // };
 
   return (
     <section className={styles.maker}>
